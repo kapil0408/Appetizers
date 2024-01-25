60eabab92c49f107dd98668362ba2d39557fc482
Kapil Goyal <kkgoyal@suyati.com>
2024-01-25 16:32:49 +0530

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Kapil Kumar on 25/01/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
