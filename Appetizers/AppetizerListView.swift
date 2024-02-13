169daf6a0a8495be8a9aed46bf6013450392d089
Kapil Goyal <kkgoyal@suyati.com>
2024-02-13 16:30:27 +0530

    Add NetworkManager and APError file

---
+++ Appetizers/AppetizerListView.swift
@@ -0,0 +1,20 @@
+//
+//  AppetizerListView.swift
+//  Appetizers
+//
+//  Created by Kapil Kumar on 13/02/24.
+//
+
+import SwiftUI
+
+struct AppetizerListView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct AppetizerListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppetizerListView()
+    }
+}
